f95355b164b5ca9135ac4fef6af7bedcdc0c8b78
zix <zabel11nakar11na@gmail.com>
2024-10-07 22:00:51 +0200

    added image settings

--- catalog/models.py
+++ catalog/models.py
@@ -5,11 +5,7 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=100, verbose_name='Имя', help_text="Введите название.")
     description = models.TextField(verbose_name='Описание', help_text="Введите описание.")
-    image = models.ImageField(
-        upload_to='product/photo',
-        verbose_name='Фото',
-        help_text="Загрузите изображение продукта."
-    )
+    image = models.ImageField(upload_to='images/', blank=True, null=True)
     category = models.ForeignKey('Category', on_delete=models.CASCADE, verbose_name='Категория')
     price = models.IntegerField(
         validators=[MinValueValidator(1)],
--- catalog/views.py
+++ catalog/views.py
@@ -8,7 +8,7 @@ def main_page(request):
     context = {'products': products}
     return render(request, 'cards_list.html', context)
 
-def product_page(request):
+def product_page(request, pk):
     product = get_object_or_404(Product, pk=pk)
     context = {'product': product}
     return render(request, 'product_page.html', context)
