77aef18cc98f120b2102398aefddf9a26fb63c34
zix <zabel11nakar11na@gmail.com>
2024-10-25 20:28:02 +0200

    password reset

--- users/views.py
+++ users/views.py
@@ -1,5 +1,9 @@
+import string
+from random import random
+
+from django.contrib.auth.hashers import make_password
 from django.core.mail import send_mail
-from django.shortcuts import get_object_or_404, redirect
+from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView
 
@@ -35,4 +39,32 @@ def email_verification(request, token):
     user = get_object_or_404(User, token=token)
     user.is_active = True
     return redirect(reverse('users:login'))
+
+from django.contrib import messages
+
+from django.contrib import messages
+
+def password_reset_view(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        if email:
+            new_password = ''.join(random.choices(string.ascii_letters + string.digits, k=12))
+            user = User.objects.filter(email=email).first()
+            if user:
+                user.password = make_password(new_password)
+                user.save()
+                send_mail(
+                    'Восстановление пароля',
+                    f'Ваш новый пароль: {new_password}',
+                    'koteika.koteevitch@yandex.ru',
+                    [email],
+                    fail_silently=False
+                )
+                messages.success(request, 'Новый пароль отправлен на ваш email.')
+            else:
+                messages.info(request, 'Пользователь с таким email не найден. Если вы считаете, что это ошибка, свяжитесь с поддержкой.')
+        else:
+            messages.error(request, 'Введите корректный email.')
+
+    return redirect(reverse('login'))
 
